1acbc5a91061a0da6df3772727bc370c9b299167
Sergey Ukhlivanov <ukhlivanov@gmail.com>
2018-12-12 18:02:08 -0800

    Commit version 2

--- src/components/landingPage-about.js
+++ src/components/landingPage-about.js
@@ -11,10 +11,13 @@ export default class LandingPageAbout extends React.Component{
                   <i className="fas fa-handshake fa-9x" ></i>
                 </div>
                 <div className="aboutus">
-                  <h3>About Us</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non eos aperiam labore consectetur maiores ea magni exercitationem
-                    similique laborum sed, unde, autem vel iure doloribus aliquid. Aspernatur explicabo consectetur consequatur non
-                    nesciunt debitis quos alias soluta, ratione, ipsa officia reiciendis.</p>
+                  <h3>Our mission</h3>
+                  <p>
+                    However, often when changing jobs, we think not only about changing the company, but also about changing professional activities. And it is precisely in these moments that the question arises: “How now to find a good job? And most importantly, what should this work be? ”
+                    To solve such issues quickly and easily, just go to CareerLine!
+                    It is convenient to look for a job on our website with user-friendly interface: just a few a mouse click and you will receive a list of current and high-quality jobs.
+                    Our web application uses the GitHub Jobs API.
+                  </p>
                 </div>
               </div>
             </div>
